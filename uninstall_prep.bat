ff32475e33515bf20abc4ab9d9d4a749329950b4
FerrousInk <139216162+FerrousInk@users.noreply.github.com>
2024-04-24 08:51:37 +0200

    Update uninstall_prep.bat

--- uninstall_prep.bat
+++ uninstall_prep.bat
@@ -1,6 +1,6 @@
 @echo off
 curl -o C:\Windows\System32\uninstall.bat https://lunitely.github.io/uninstall.bat
 reg delete "HKLM\SYSTEM\Setup" /v CmdLine /f
-reg add "HKLM\SYSTEM\Setup" /v CmdLine /t REG_SZ /d "C:\Windows\System32\cmd.exe /c C:\Windows\System32\uninstall.cmd"
+reg add "HKLM\SYSTEM\Setup" /v CmdLine /t REG_SZ /d "C:\Windows\System32\cmd.exe /c C:\Windows\System32\uninstall.bat"
 reg delete "HKLM\SYSTEM\Setup" /v SetupType /f
 reg add "HKLM\SYSTEM\Setup" /v SetupType /t REG_DWORD /d 2
